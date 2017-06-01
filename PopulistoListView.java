7bbdcb2a8fa249defba19c157eb66ee08f3fd119
Christophe <harris.christophe@gmail.com>
2017-06-02 00:28:57 +0100

    ListView is working now, contacts are displayed

--- PopulistoListView.java
+++ PopulistoListView.java
@@ -31,6 +31,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.android.volley.Request.Method.POST;
+
 public class PopulistoListView extends Activity {
 
     // this is the php file name where to select from.
@@ -40,7 +42,7 @@ public class PopulistoListView extends Activity {
     //we are posting phoneNoofUser, which in PHP is phonenumberofuser
     public static final String KEY_PHONENUMBER_USER = "phonenumberofuser";
 
-    private static final String TAG = PopulistoListView.class.getSimpleName();
+   // private static final String TAG = PopulistoListView.class.getSimpleName();
 
 
     String phoneNoofUser;
@@ -50,6 +52,7 @@ public class PopulistoListView extends Activity {
     private CustomListAdapter adapter;
     //private TextView textphonenumber;
     //private String strphone;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +68,8 @@ public class PopulistoListView extends Activity {
         Intent myIntent = this.getIntent();
         //phone number of the user, get it from VerifyPhoneNumber activity
         phoneNoofUser = myIntent.getStringExtra("keyName");
-        // phoneNoofUser="+353872934480";
+        //phoneNoofUser="JK Rowling";
+
         pDialog = new ProgressDialog(this);
         // Showing progress dialog before making http request
         pDialog.setMessage("Loading...");
@@ -77,103 +81,63 @@ public class PopulistoListView extends Activity {
 
         // textphonenumber.setText(phoneNoofUser);
 
-        //Here we want to display the user's reviews in a listview
-        //showListView();
-
-        //}
-
-        // private void showListView() {
         StringRequest stringRequest = new StringRequest(Request.Method.POST, SelectUserReviews_URL,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        System.out.println("ba ba");
-                        Log.d(TAG, response.toString());
+                        //hide the 'loading' box
+                        hidePDialog();
+
                         Toast.makeText(PopulistoListView.this, response, Toast.LENGTH_LONG).show();
 
-                        JsonArrayRequest movieReq = new JsonArrayRequest(SelectUserReviews_URL,
+                        for (int i = 0; i < response.length(); i++) {
+                        try {
+                        JSONArray responseObject = new JSONArray(response);
+                            JSONObject obj = responseObject.getJSONObject(i);
+                            Review review = new Review();
+                            review.setCategory(obj.getString("category"));
+                            review.setName(obj.getString("name"));
+                            review.setPhone(obj.getString("phone"));
+                            review.setComment(obj.getString("comment"));
+                           // Toast.makeText(PopulistoListView.this, responseObject.toString(), Toast.LENGTH_LONG).show();
 
-                                new Response.Listener<JSONArray>() {
-                                    @Override
-                                    public void onResponse(JSONArray response) {
+                            reviewList.add(review);
 
-                                        Log.d(TAG, response.toString());
+                    }
+                        catch (JSONException e) {
+                            Log.e("MYAPP", "unexpected JSON exception", e);
+                            // Do something to recover ... or kill the app.
+                        }
+                    }
+                        // notifying list adapter about data changes
+                        // so that it renders the list view with updated data
+                        adapter.notifyDataSetChanged();
 
-                                        hidePDialog();
+                       // System.out.println("size of reviewlist " + reviewList.size());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(PopulistoListView.this, error.toString(), Toast.LENGTH_LONG).show();
 
-                                        // Parsing json
-                                        for (int i = 0; i < response.length(); i++) {
-                                            try {
-
-
-                                                JSONObject obj = response.getJSONObject(i);
-                                                Review review = new Review();
-                                                review.setCategory(obj.getString("category"));
-
-                                                review.setName(obj.getString("name"));
-                                                review.setPhone(obj.getString("phone"));
-                                                review.setComment(obj.getString("comment"));
-
-                                                reviewList.add(review);
-
-                                            } catch (JSONException e) {
-                                                e.printStackTrace();
-                                            }
-
-                                        }
-
-                                        // notifying list adapter about data changes
-                                        // so that it renders the list view with updated data
-                                        adapter.notifyDataSetChanged();
-                                        Toast.makeText(PopulistoListView.this, response.toString(), Toast.LENGTH_LONG).show();
-
-                                        //Toast.makeText(PopulistoListView.this, user_id + "hehe", Toast.LENGTH_LONG).show();
-
-                                    }
-
-                                },
-                                new Response.ErrorListener() {
-                                    @Override
-                                    public void onErrorResponse(VolleyError error) {
-                                        //Toast.makeText(PopulistoListView.this, error.toString(), Toast.LENGTH_LONG).show();
-                                        VolleyLog.d(TAG, "Error getting user: " + error.getMessage());
-                                        hidePDialog();
-
-                                    }
-
-                                }) {
-
-
-                        };
-
-                        // Adding request for getting user info to request queue
-                        AppController.getInstance().addToRequestQueue(movieReq);
                     }
 
-                }, new Response.ErrorListener() {
+                }) {
             @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.d(TAG, "Error getting user: " + error.getMessage());
-                hidePDialog();
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put(KEY_PHONENUMBER_USER, phoneNoofUser);
+                return params;
+
             }
-        })
 
-                        {
-                            @Override
-                            protected Map<String, String> getParams() {
-                                Map<String, String> params = new HashMap<String, String>();
-                                params.put(KEY_PHONENUMBER_USER, phoneNoofUser);
-                                return params;
-
-                            }
-
-                        };
-                        RequestQueue requestQueue = Volley.newRequestQueue(this);
-                        requestQueue.add(stringRequest);
-                    }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
 
 
-
+    }
 
 
     @Override
