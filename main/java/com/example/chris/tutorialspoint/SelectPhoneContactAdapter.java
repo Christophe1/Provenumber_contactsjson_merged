2c8d56be674a998e74d3d4bc1f05c2cac7a133bd
Christophe1 <harris.christophe@gmail.com>
2017-09-14 23:28:12 +0100

    following the code in this SO question : http://bit.ly/2jsHp2B

--- main/java/com/example/chris/tutorialspoint/SelectPhoneContactAdapter.java
+++ main/java/com/example/chris/tutorialspoint/SelectPhoneContactAdapter.java
@@ -3,26 +3,18 @@ package com.example.chris.tutorialspoint;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.tutorialspoint.R;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-
-import static com.example.tutorialspoint.R.id.phone;
 
 /**
  * Created by Chris on 23/06/2017.
@@ -34,21 +26,24 @@ public class SelectPhoneContactAdapter extends BaseAdapter {
     public List<SelectPhoneContact> theContactsList;
     //define an array list made out of SelectContacts and call it arraylist
     private ArrayList<SelectPhoneContact> arraylist;
-  //  boolean mChecked[];
- //   boolean itemChecked[];
-    Context _c;
+
+    Context context;
+    ArrayList<HashMap<String, String>> stuff;
+    HashMap<String, String> resultp = new HashMap<String, String>();
 
 
     //define a ViewHolder to hold our name and number info, instead of constantly querying
     // findviewbyid. Makes the ListView run smoother
     ViewHolder v;
 
-    public SelectPhoneContactAdapter(final List<SelectPhoneContact> selectPhoneContacts, Context context) {
+    public SelectPhoneContactAdapter(final List<SelectPhoneContact> selectPhoneContacts, Context context,
+                                     ArrayList<HashMap<String, String>> arraylistofstuff) {
+
         theContactsList = selectPhoneContacts;
-        _c = context;
+        this.context = context;
         this.arraylist = new ArrayList<SelectPhoneContact>();
         this.arraylist.addAll(theContactsList);
-      //  itemChecked = new boolean[theContactsList.size()];
+        stuff = arraylistofstuff;
 
     }
 
@@ -56,17 +51,19 @@ public class SelectPhoneContactAdapter extends BaseAdapter {
     @Override
     public int getCount() {
         System.out.println("the amount in arraylist :" + arraylist.size());
-        return arraylist.size();
+        return stuff.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return theContactsList.get(i);
+        return null;
+        //return theContactsList.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return i;
+        return 0;
+       // return i;
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
@@ -79,7 +76,7 @@ public class SelectPhoneContactAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView( int i, View convertView, ViewGroup viewGroup) {
+    public View getView( int i, View convertView, ViewGroup parent) {
 
         //we're naming our convertView as view
         View view = convertView;
@@ -91,44 +88,23 @@ public class SelectPhoneContactAdapter extends BaseAdapter {
 
 
             //if there is nothing there (if it's null) inflate the view with the layout
-            LayoutInflater li = (LayoutInflater) _c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LayoutInflater li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = li.inflate(R.layout.phone_inflate_listview, null);
 
+            // Get the position
+            resultp = stuff.get(i);
 
             //      So, for example, title is cast to the name id, in phone_inflate_listview,
 //        phone is cast to the id called no etc
             v.title = (TextView) view.findViewById(R.id.name);
             v.phone = (TextView) view.findViewById(R.id.no);
             v.check = (CheckBox) view.findViewById(R.id.checkBoxContact);
-            //       v.lookup = (TextView) view.findViewById(R.id.lookup);
-//       v.check.setTag(Integer.valueOf(i)); // set the tag so we can identify the correct row in the listener
-//        v.check.setChecked(mCheckStates.get(i, false)); // set the status as we stored it
-//        v.check.setOnCheckedChangeListener(mListener); // set the listener
 
 
 
 
             view.setTag(v);
 
-/*
-            v.title.setOnClickListener( new View.OnClickListener() {
-                public void onClick(View v) {
-                    CheckBox cb = (CheckBox) v ;
-                    SelectPhoneContact selectPhoneContact = (SelectPhoneContact) cb.getTag();
-*/
-/*                    Toast.makeText(getApplicationContext(),
-                            "Clicked on Checkbox: " + cb.getText() +
-                                    " is " + cb.isChecked(),
-                            Toast.LENGTH_LONG).show();*//*
-
-                    selectPhoneContact.setSelected(cb.isChecked());
-                }
-            });
-*/
-
-            // v.check.setOnCheckedChangeListener((NewContact) _c);
-
-            //or else use the view (what we can see in each row) that is already there
         } else {
             view = convertView;
 
@@ -140,38 +116,17 @@ public class SelectPhoneContactAdapter extends BaseAdapter {
 
         //in the listview for contacts, set the name
         v.title.setText(data.getName());
-       // v.title.setText(getItem(i));
         //in the listview for contacts, set the number
         v.phone.setText(data.getPhone());
 
 
-        v.check.setChecked(false);
-/*
-        v.check.setChecked(itemChecked[i]);
-
-        v.check
-                .setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton buttonView,
-                                                 boolean isChecked) {
-                        itemChecked[i] = isChecked;
-                    }
-                });*/
-
-         //view.setTag(data);
+        //v.check.setChecked(false);
 
         // Return the completed view to render on screen
         return view;
 
     }
 
-/*    CompoundButton.OnCheckedChangeListener mListener = new CompoundButton.OnCheckedChangeListener() {
-
-        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            mChecked[(Integer)buttonView.getTag()] = isChecked; // get the tag so we know the row and store the status
-            System.out.println("getview position :" + mChecked);
-        }
-    };*/
 
     }
 
