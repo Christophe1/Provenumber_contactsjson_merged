5b7482ba24d3f6c7f2832627c0d4477e8392efef
Christophe1 <harris.christophe@gmail.com>
2018-02-02 07:55:00 +0000

    trying to embed fragment into NewContact, not working yet

---
+++ main/java/com/example/chris/tutorialspoint/button_sharing.java
@@ -0,0 +1,35 @@
+package com.example.chris.tutorialspoint;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tutorialspoint.R;
+
+/**
+ * Created by Chris on 01/02/2018.
+ */
+
+public class button_sharing extends Fragment{
+
+
+    // The onCreateView method is called when Fragment should create its View object hierarchy,
+    // either dynamically or via XML layout inflation.
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.sharing_buttons, parent, false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    }
+
+}
