2805008f47ab78e8bf9f34d79b23765897ae58c2
Christophe1 <harris.christophe@gmail.com>
2019-12-16 23:25:27 +0000

    trying to fix Places in EditContact

--- main/java/com/populisto/chris/populisto/EditContact.java
+++ main/java/com/populisto/chris/populisto/EditContact.java
@@ -45,6 +45,11 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.populisto.chris.populisto.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -57,6 +62,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -118,7 +124,7 @@ public class EditContact extends AppCompatActivity implements GoogleApiClient.On
   private ArrayList<CategoryList> existingCategoryList;
 
 
-  private PlaceAutoCompleteAdapter mPlaceAutoCompleteAdapter;
+ // private PlaceAutoCompleteAdapter mPlaceAutoCompleteAdapter;
 
   //for if review can be saved, nees fields filled for Public and Phone Contacts
   Boolean allValid;
@@ -173,6 +179,46 @@ public class EditContact extends AppCompatActivity implements GoogleApiClient.On
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_edit_contact);
 
+    Places.initialize(getApplicationContext(),getString(R.string.places_api_key));
+    // Create a new Places client instance.
+    PlacesClient placesClient = Places.createClient(this);
+
+    // Initialize the AutocompleteSupportFragment.
+    AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+        getSupportFragmentManager().findFragmentById(R.id.textViewCategory);
+
+    // Specify the types of place data to return.
+/*    autocompleteFragment.setPlaceFields(Arrays.asList(
+        Place.Field.NAME,
+        Place.Field.LAT_LNG
+    ));*/
+
+    // Set up a PlaceSelectionListener to handle the response.
+/*    autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+      @Override
+      public void onPlaceSelected(Place place) {
+        // TODO: Get info about the selected place.
+        String name = place.getName();
+        double lat, lng;
+        if (place.getLatLng() !=null){
+          lat =place.getLatLng().latitude;
+          lng =place.getLatLng().longitude;
+        }
+
+        //do something
+      }
+
+      @Override
+      public void onError(Status status) {
+        // TODO: Handle the error.
+        // Log.i(TAG, "An error occurred: " + status);
+      }
+    });*/
+
+
+
+
+
     noContactsFound = (TextView) findViewById(R.id.noContactsFoundView);
 
     existingCategoryList = new ArrayList<>();
@@ -186,17 +232,17 @@ public class EditContact extends AppCompatActivity implements GoogleApiClient.On
     PopulistoContactsAdapter adapter = new PopulistoContactsAdapter(selectPhoneContacts, EditContact.this, 2);
 
     // Create a GoogleApiClient instance
-    mGoogleApiClient = new GoogleApiClient
+ /*   mGoogleApiClient = new GoogleApiClient
         .Builder(this)
         .addApi(Places.GEO_DATA_API)
         .addApi(Places.PLACE_DETECTION_API)
         .enableAutoManage(this, 0, this)
-        .build();
+        .build();*/
 
     //Google Places
-    mPlaceAutoCompleteAdapter = new PlaceAutoCompleteAdapter(this, mGoogleApiClient, LAT_LNG_BOUNDS, null);
+    //mPlaceAutoCompleteAdapter = new PlaceAutoCompleteAdapter(this, mGoogleApiClient, LAT_LNG_BOUNDS, null);
 
-    AutoCompTextViewAddress.setAdapter(mPlaceAutoCompleteAdapter);
+    //AutoCompTextViewAddress.setAdapter(mPlaceAutoCompleteAdapter);
 
 
     //put in a toolbar
